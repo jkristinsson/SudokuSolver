0373d2c86340c79a86f67f8c8c9dc33ff341119f
Johannes Kristinsson <johannes@kristinsson.se>
2025-01-18 20:58:21 -0500

    Added lines for reading puzzles

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -2,6 +2,19 @@ import curses
 from CursesSudoku import *
 
 
+# Parse input file
+with open("Puzzles/NYT20250117Easy.txt", "r") as f:
+    lines = f.read().strip().split('\n')
+    f.close()
+
+b = SudokuBoard()
+for (r,row) in enumerate(lines):
+    for (c,char) in enumerate(row):
+        if char != '.' : 
+            x = int(char)
+            sc = SudokuCoordinate(r+1, c+1)
+            b.setCell(sc, x)
+
 
 
 
@@ -16,7 +29,7 @@ def main(scr : curses.window):
     #curses.init_pair(cBorder, fg=curses.COLOR_BLACK, bg=curses.COLOR_WHITE)
     #curses.init_pair(cPossible, fg=curses.co)
 
-    b = SudokuBoard()
+    #b = SudokuBoard()
 
     while True:
         printGrid(scr, b)
