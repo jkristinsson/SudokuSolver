2e0267fad599ceb2174c035844b64e4e26a776cd
jkristinsson <46459484+jkristinsson@users.noreply.github.com>
2025-01-18 22:33:57 -0500

    Added better support for numbers and to finalize cells

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -16,7 +16,7 @@ for (r,row) in enumerate(lines):
             b.setCell(sc, x)
 
 
-
+ARROW_KEYS = {curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN}
 
 def main(scr : curses.window):
     # Clear screen
@@ -31,18 +31,32 @@ def main(scr : curses.window):
 
         if k == ord('q') : 
             break # Exit the program
-        
-        elif k == ord(' ') : 
+
+        elif k == curses.KEY_ENTER or k == 10 :
+            finalizeCell(scr, b)
+            continue
+            
+        elif k == ord('h') :
+            getHighlight(scr, b)
+            continue
+
+        elif k == ord('c') : 
             if b.cursorCell == None :
                 b.cursorCell = SudokuCoordinate(5,5) 
             else :
                 b.cursorCell = None
+            continue
 
-        elif b.cursorCell != None :
+        elif b.cursorCell != None and k in ARROW_KEYS :
             if   k == curses.KEY_LEFT  : b.cursorCell.moveLeft()
             elif k == curses.KEY_RIGHT : b.cursorCell.moveRight()
             elif k == curses.KEY_UP    : b.cursorCell.moveUp()
             elif k == curses.KEY_DOWN  : b.cursorCell.moveDown()
+            continue
+
+        else :
+            setMessage(scr, f"Key {k} not recognized")
+
 
 
 curses.wrapper(main)
